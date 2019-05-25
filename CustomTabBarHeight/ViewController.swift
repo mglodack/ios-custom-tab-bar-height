49fda7dc45c082028acf8946f5137844cabf5d4d
Mathew Glodack <glodack@gmail.com>
2019-05-25 09:40:17 -0700

    chore: Initial project setup

---
+++ CustomTabBarHeight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTabBarHeight
+//
+//  Created by Mathew Glodack on 5/25/19.
+//  Copyright © 2019 glodack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+}
+
